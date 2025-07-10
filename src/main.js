b97d1901ae85f41471ee6891aeae2ab48165fa12
najwadea <najwadea18@gmail.com>
2025-07-10 14:49:25 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './assets/main.css'
 import './bootstrap-icons/font/bootstrap-icons.css'
 
-import { createApp } from 'vue'
+import { createApp } from './vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
